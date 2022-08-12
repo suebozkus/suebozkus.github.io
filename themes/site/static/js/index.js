84d95f86edc2a7187c8f9e1a8597ee42a7fad201
suebozkus <75066493+suebozkus@users.noreply.github.com>
2022-08-12 11:23:22 +0200

    Toggle Dark Theme

--- themes/site/static/js/index.js
+++ themes/site/static/js/index.js
@@ -1,9 +1,10 @@
-console.log("xxx")
-
 // TODO 1. Select Element with Javascript.
 // TODO 2. After selection, add event listener.
 // TODO 3. Select body element and toggle class to the body.
 // TODO 4. Add some css for the dark theme of this class.
 
 const toggler = document.getElementById("toggletheme");
-console.log(toggler);
+toggler.addEventListener ("change", (e) => {
+    const body = document.getElementById("body");
+    body.classList.toggle("darktheme");
+})
