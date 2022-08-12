57476e1f1c248d281a5f5d1ef463b04f2b27d521
suebozkus <75066493+suebozkus@users.noreply.github.com>
2022-08-12 11:06:07 +0200

    Run js after render

--- themes/site/static/js/index.js
+++ themes/site/static/js/index.js
@@ -1,1 +1,9 @@
 console.log("xxx")
+
+// TODO 1. Select Element with Javascript.
+// TODO 2. After selection, add event listener.
+// TODO 3. Select body element and toggle class to the body.
+// TODO 4. Add some css for the dark theme of this class.
+
+const toggler = document.getElementById("toggletheme");
+console.log(toggler);
