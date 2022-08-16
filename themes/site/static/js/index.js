3c40610c16549622066acf545b00571b9f5cd1a5
suebozkus <75066493+suebozkus@users.noreply.github.com>
2022-08-16 11:36:51 +0200

    Fade in Container on Scroll

--- themes/site/static/js/index.js
+++ themes/site/static/js/index.js
@@ -8,3 +8,18 @@ toggler.addEventListener ("change", (e) => {
     const body = document.getElementById("body");
     body.classList.toggle("darktheme");
 })
+
+// TODO 1. Hide the Elements.
+// TODO 2. Scroll Event listener.
+// TODO 3. Calculate off-set of the blocks and define threshhold when to show.
+// TODO 4. Show the elements.
+// TODO 5. Trigger the reveal effect in initial offset.
+
+document.addEventListener('scroll', (e) => { 
+    if (window.scrollY + window.innerHeight - 300 > document.getElementById("projects").offsetTop)  {
+        document.getElementById("projects").classList.remove("opacity-0")
+    } 
+    if (window.scrollY + window.innerHeight - 300 > document.getElementById("contact").offsetTop)  {
+        document.getElementById("contact").classList.remove("opacity-0")
+    } 
+})
